61090b19a60d696508970097ffa63fda33592200
Vasileios Mitrousis <vamitrou@gmail.com>
2015-11-03 17:25:25 +0100

    files restructure

---
+++ pia-oracle.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func trigger(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Triggered..")
+	go importData()
+}
+
+func callback(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "callback")
+	go exportData()
+}
+
+func importData() {
+	fmt.Printf("importing data\n")
+	odb := new(OracleDB)
+	odb.ReadConfig("conf/db_conf.json")
+	fmt.Println(odb.Conf.Host)
+	odb.GetData()
+}
+
+func exportData() {
+	time.Sleep(10 * time.Second)
+	fmt.Printf("exporting data\n")
+}
+
+func main() {
+	fmt.Println("Server started")
+
+	rest := new(Rester)
+	rest.ReadConfig("conf/rest_conf.json")
+
+	http.HandleFunc("/trigger", trigger)
+	http.HandleFunc("/callback", callback)
+	http.ListenAndServe(":8001", nil)
+}
